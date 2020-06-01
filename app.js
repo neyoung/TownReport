1ca9b2732fa8c2cfc973d25f8aec6346e9d22fad
neyoung <ngozi.eze.young@gmail.com>
2020-05-31 23:44:12 -0400

    Optimized code by 28 lines using Udemy's solution

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-// TODO: These global variables are not secure
-const ageOfParks = [];
-const treesInPark = new Map();
-const streetLengths = [];
-
 class Town {
   constructor(name, buildYear, length) {
     this.name = name;
@@ -16,16 +11,23 @@ class Town {
   calcAge() {
     return new Date().getFullYear() - this.buildYear;
   }
+
+  // Utility method that takes an array and returns the sum and average
+  static calc(arr) {
+    const sum = arr.reduce((prev, cur) => prev + cur);
+    // returns sum and average as an array
+    return [sum, sum / arr.length];
+  }
 }
 
 class Street extends Town {
   constructor(name, buildYear, length) {
     super(name, buildYear, length);
 
-    // Save street length into an array
-    streetLengths.push(this.length);
-
-    // Detemine size classification of street based on its length
+    this.classifyStreet();
+  }
+  // Detemine size classification of street based on its length
+  classifyStreet() {
     let x = this.length;
     if (x > 0 && x <= 500) {
       this.sizeCategory = "tiny";
@@ -39,19 +41,6 @@ class Street extends Town {
       this.sizeCategory = 'huge';
     }
   }
-
-  static calcTotalLengths() {
-    let totalLength = 0;
-    for (let i = 0; i < streetLengths.length; i++) {
-      totalLength += streetLengths[i];
-    }
-    return totalLength;
-  }
-
-  static calcAvgLength() {
-    return Street.calcTotalLengths()/streetLengths.length;
-  }
-
 }
 
 class Park extends Town {
@@ -60,26 +49,7 @@ class Park extends Town {
     this.width = width;
     this.numOfTrees = numOfTrees;
 
-    // This is immediately called when the Park class is instantiated.
-    // It stores data about Park objects to global data structures.
-    ageOfParks.push(this.calcAge());
-    // console.log(ageOfParks);
-    treesInPark.set(this.name, this.numOfTrees);
-    // console.log(treesInPark);
   }
-
-    // Calculates the total age of all Parks
-    static sumOfAges() {
-      let sum = 0;
-      for (let i = 0; i < ageOfParks.length; i++) {
-        sum += ageOfParks[i];
-      }
-      return sum;
-    }
-
-    static calcAvgAge() {
-      return Park.sumOfAges()/ageOfParks.length;
-    }
 
     calcParkArea() {
       return this.length * this.width;
@@ -90,45 +60,47 @@ class Park extends Town {
     }
 
     // Name of parks with number of trees greater than 1000
-    static withLotsOfTrees() {
-      let s = '';
-      for (let [parkName, numOfTrees] of treesInPark.entries()) {
-        if (numOfTrees > 1000) {
-          console.log(`${parkName} park has more than 1000 trees.`);
+    static withLotsOfTrees(arr) {
+      arr.map(el => {
+        if (el.numOfTrees > 1000) {
+          console.log(`${el.name} park has more than 1000 trees.`);
         }
-      }
+      });
     }
 
   }
 
-const laurelStreets = [];
-const laurelParks = [];
 // Create four street objects and save to an array
-let street1, street2, street3, street4;
-laurelStreets.push(street1 = new Street('Gramercy Lane', 1900, 500));
-laurelStreets.push(street2 = new Street('East Gate Lane', 1890, 1000));
-laurelStreets.push(street3 = new Street('Key West Avenue', 1870, 2000));
-laurelStreets.push(street4 = new Street('Main Avenue', 1850, 3000));
+const allStreets = [new Street('Gramercy Lane', 1900, 500),
+                      new Street('East Gate Lane', 1890, 1000),
+                      new Street('Key West Avenue', 1870, 2000),
+                      new Street('Main Avenue', 1850, 3000)];
 
 // Create three park objects and save to an array
-let park1, park2, park3;
-laurelParks.push(park1 = new Park('Assateague State Park', 1956, 1000, 500, 100));
-laurelParks.push(park2 = new Park('Chapman State Park', 1998, 500, 250, 2000));
-laurelParks.push(park3 = new Park('Elk Neck State Park', 1936, 2000, 1000, 3000));
+const allParks = [new Park('Assateague State Park', 1956, 1000, 500, 100),
+                    new Park('Chapman State Park', 1998, 500, 250, 2000),
+                    new Park('Elk Neck State Park', 1936, 2000, 1000, 3000)];
 
-// Prints The Report to console
-console.log('----PARKS REPORT----')
-console.log(`Our ${laurelParks.length} parks have an average of ${Park.calcAvgAge()} years.`);
-
-for(let i = 0; i < laurelParks.length; i++) {
-  console.log(`${laurelParks[i].name} has a tree density of ${laurelParks[i].calcTreeDensity()} trees per square km.`);
+function reportParks(p) {
+  console.log('----PARKS REPORT----')
+  // Loops through array to calculate the ages and stored them to an array, ages
+  const ages = p.map(el => el.calcAge());
+  // Using decompsition to save the returned total age and average age
+  const [totalAge, avgAge] = Park.calc(ages);
+  console.log(`Our ${p.length} parks have an average of ${avgAge} years.`);
+  p.map(el => console.log(`${el.name} has a tree density of ${el.calcTreeDensity()} trees per square km.`));
+  Park.withLotsOfTrees(p); // prints to console
 }
 
-Park.withLotsOfTrees(); // prints to console
+function reportStreets(s) {
+  console.log('----STREETS REPORT----')
+  const lengths = s.map(el => el.length);
+  const [totalLength, avgLength] = Street.calc(lengths);
+  console.log(`Our ${s.length} streets have a total length of ${totalLength} km, with an average of ${avgLength} km.`);
+  s.map(el => console.log(`${el.name}, built in ${el.buildYear}, is a ${el.sizeCategory} street.`));
 
-console.log('----STREETS REPORT----')
-console.log(`Our ${laurelStreets.length} streets have a total length of ${Street.calcTotalLengths()} km, with an average of ${Street.calcAvgLength()} km.`);
-
-for(let i = 0; i < laurelStreets.length; i++) {
-  console.log(`${laurelStreets[i].name}, built in ${laurelStreets[i].buildYear}, is a ${laurelStreets[i].sizeCategory} street.`);
 }
+
+// Calls the methods that prints the report to console
+reportParks(allParks);
+reportStreets(allStreets);
